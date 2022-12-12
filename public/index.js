f255887dad4cb0d0ec8b153f27db06cbbad95648
gracieArmour <graciearmour42@gmail.com>
2022-12-11 17:37:13 -0800

    Completed logic for starting and connecting to the character database

--- public/index.js
+++ public/index.js
@@ -3,15 +3,23 @@ console.log("client-side js loaded");
 var formElem = document.getElementById("character-sheet");
 var submitButton = document.getElementById("saveButton");
 
-submitButton.addEventListener('click',e => {
-    e.preventDefault();
-    var o = {};
-    new FormData( formElem ).forEach(( value, key ) => o[key] = value );
-    fetch('/save_character', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(o)
+// submitButton.addEventListener('click',e => {
+//     e.preventDefault();
+//     var o = {};
+//     new FormData( formElem ).forEach(( value, key ) => o[key] = value );
+//     fetch('/save_character', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(o)
+//     })
+// })
+
+var header = document.getElementById("site-header");
+
+header.addEventListener('click', e => {
+    fetch('/database', {
+        method: 'POST'
     })
-})
+});
--- server.js
+++ server.js
@@ -3,13 +3,22 @@
 */
 
 // dependencies
+require('dotenv').config();
 const express = require('express');
 const exhandle = require('express-handlebars');
+const mysql = require('mysql');
 const fs = require('fs');
 const path = require('path');
 
-// set port value
+// collect environmentally stored variables
 var port = process.env.PORT || 3000;
+
+const db = {
+  host: process.env.DBADDRESS,
+  user: process.env.DBUSER,
+  password: process.env.DBPASS,
+  database: process.env.DBNAME
+};
 
 // set up express for use with handlebars
 const app = express();
@@ -24,6 +33,9 @@ app.set('view engine', 'handlebars');
 // var characterFilenames = fs.readdirSync(path.join(__dirname,'character_data'));
 // characterFilenames.forEach
 // var postData = JSON.parse(fs.readFileSync("./postData.json"));
+
+// connect to character database
+const connection = mysql.createConnection(db);
 
 // get list of current systems pages
 var systemsList = fs.readdirSync(path.join(__dirname,'views','systems'));
@@ -40,18 +52,37 @@ app.get('/:homePath(home|index|index.html)?', function(req, res) {
 app.get('/systems/:sys', function(req, res) {
   var sys = req.params.sys;
   if (systemsList.includes(sys)) {
-    res.status(200).render(path.join('systems',sys), { layout: 'system' });
+    res.status(200).render(path.join('systems',sys), {
+      layout: 'system'
+    });
   }else {
     res.status(404).render('404');
   }
 });
 
 // catch form data
-app.post('/save_character', express.json(), function( req, res ) {
+app.post('/save_character', express.json(), function(req, res) {
   console.log(req.body);
   res.status(200).send("post successful");
   res.end();
 });
+
+app.post('/database', function(req, res) {
+  connection.connect();
+
+  connection.query('SELECT * FROM test', (err, rows, fields) => {
+    if (err) {
+      console.log(err);
+    }else {
+      rows.forEach(row => {
+        console.log("ID: "+row.id+", NAME: "+row.name);
+      });
+    }
+  })
+
+  connection.end();
+  res.status(200).send("posted");
+})
 
 // routing for 404 error page
 app.use((req, res) => {res.status(404).render('404')});
